65b1c81dd66437de120c471c0799f3a2801be939
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:23:20 -0400

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//declared then intialized
+char letter;
+letter = 'A';
+//declared then intialized
+int number;
+number = 100;
+//declared and initialized
+float body = 98.6f;
+//declared and initialized
+double pi = 3.14159;
+//declared and initialized
+decimal sum = 1000.00m;
+//declared and initialized
+bool flag = false;
+//declared and initialized
+string text = "C# is fun!";
+
+Console.WriteLine("char letter:\t" + letter);
+Console.WriteLine("int number:\t" + number);
+Console.WriteLine("float body:\t" + body);
+Console.WriteLine("double pi:\t" + pi);
+Console.WriteLine("decimal sum:\t" + sum);
+Console.WriteLine("bool flag:\t" + flag);
+Console.WriteLine("string text:\t" + text);
+Console.ReadKey();
